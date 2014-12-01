a201e07054818b2bb910276397a8795aed9da546
jthang <admin@Jens-MacBook-Air.local>
2014-12-01 17:01:34 +0800

    Update to output dir

--- publishconf.py
+++ publishconf.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
-import os
 import sys
-sys.path.append(os.curdir)
+sys.path.append('.')
 from pelicanconf import *
 
-SITEURL = ''
-RELATIVE_URLS = False
-
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+SITEURL = 'https://jthang.github.io/'
 
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# Uncomment following line for absolute URLs in production:
+#RELATIVE_URLS = False
+
+GOOGLE_ANALYTICS = ''
+DISQUS_SITENAME = ''
